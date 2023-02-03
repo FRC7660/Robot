4e250284694ca2534322aa52f558302142a0f743
erclee2000 <98067054+erclee2000@users.noreply.github.com>
2023-02-03 09:52:05 -0500

    blank template for students to start with

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,12 +9,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.Autos;
-import frc.robot.commands.DriveTank;
 import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.RealDrive;
-import frc.robot.subsystems.SimDrive;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,32 +20,12 @@ import frc.robot.subsystems.SimDrive;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  // Dont remove example until autons are programmed
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private Drive m_drive;
   private final Drive m_realDrive = new RealDrive();
-  private final SimDrive m_simDrive = new SimDrive();
-
-  private final XboxController leftStick = new XboxController(0);
-  private final XboxController rightStick = new XboxController(1);
-  // private final XboxController xboxController = new XboxController(2);
-
-  private Double speed = 1.0;
+  private final XboxController controller = new XboxController(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-
-    if (RobotBase.isSimulation()) {
-      m_simDrive.setDefaultCommand(
-          new DriveTank(m_simDrive, leftStick::getLeftY, leftStick::getRightY, speed));
-      m_drive = m_simDrive;
-    } else {
-      m_realDrive.setDefaultCommand(
-          new DriveTank(m_realDrive, leftStick::getLeftY, rightStick::getLeftY, speed));
-      m_drive = m_realDrive;
-    }
+    //m_realDrive.setDefaultCommand(new ArcadeDrive(//students to code this));
   }
 
   /**
@@ -70,6 +46,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
